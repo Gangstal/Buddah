8b3252e4dfc3d78eefcf54d479492e4a9c00a260
Hamster-Furtif <lakartoonerie_dutronc@yahoo.fr>
2018-01-20 19:26:08 +0100

    Improved as fuck

--- buddah.py
+++ buddah.py
@@ -1,5 +1,15 @@
 import praw
 
+file = open("buddah/conf/userdata.txt")
+data = file.read().split("\n")
+
+name   = data[0]
+pwd    = data[1]
+cid    = data[2]
+secret = data[3]
+
+secret
+
 reddit = praw.Reddit(user_agent='Comment Extraction (by /u/)'+name,
-                     client_id=id, client_secret=secret,
+                     client_id=cid, client_secret=secret,
                      username=name, password=pwd)
