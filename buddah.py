9d59f9a4b1b8c9e07825334a05cf0e5cd506b63f
Hamster-Furtif <lakartoonerie_dutronc@yahoo.fr>
2018-01-22 22:41:37 +0100

    Added Start, Stop, Pause and Clear commands

--- buddah.py
+++ buddah.py
@@ -21,24 +21,36 @@ data = file.read().split("\n")
 
 minimum_karma = int(data[0])
 sublist_acts_as_filter = data[1]=="True"
+message_refresh= int(data[2])   #Message refresh rate can be a bit longer than wanted if Buddah is processing a post
+balise = data[3]
 
 file = open("buddah/conf/admins.txt")
 admins = file.read().split("\n")
 
+RUNNING, STOPPED, PAUSED = "Running", "Stopped", "Paused"
+status = RUNNING
+
 reddit = praw.Reddit(user_agent='Comment Extraction (by /u/)'+name,
                      client_id=cid, client_secret=secret,
                      username=name, password=pwd)
+
 if(sublist_acts_as_filter):
     subreddits = reddit.subreddit("all")
 else:
     subreddits = reddit.subreddit(subs)
     
 def searchForReposts():
+    last_checked = time.time() - message_refresh
     print("Creating stream...")
     stream = subreddits.stream.submissions()
     i=0
     print("Receiving posts...")
     for post in stream:
+
+        if time.time()-last_checked >= message_refresh:
+            checkForCommands()
+            last_checked = time.time()
+        
         i+=1
         url = post.url.replace("https", "http")
         if (not sublist_acts_as_filter or not postedInFilteredSub(post)):
@@ -69,39 +81,108 @@ def searchForReposts():
 
                     else:
                         print("Best score ("+str(posts[0].score)+") is lesser than required ("+str(minimum_karma)+")")
-            
-        if(i%50==0):
-            print("Checking for new messages...")
-            messages = reddit.inbox.unread()
-            try:
-                for m in messages:
-                    if(m.author.name in admins and m.body=="STOP"):
-                        m.mark_read()
-                        m.reply("Sucessfully stoped!")
-                        print("Recieved STOP instruction from "+m.author.name)
-                        stream.close()
-                        return True
-            except AttributeError:
-                print("No new messages")
-            except StopIteration:
-                print("End of new messages")
 
-def idle():
-    while True:
-        messages = reddit.inbox.unread()
-        try:
-            for m in messages:
-                print(m.author.name, m.body)
-                if(m.author.name in admins and m.body=="START"):
+
+
+
+def checkForCommands(arg = -1):
+    print("Checking for new messages...")
+    messages = reddit.inbox.unread()
+    global status
+    try:
+        for m in messages:
+            if m.author.name in admins:
+
+                command = m.body.lower().split(" ")
+                instruction = command[0]
+                author = m.author.name
+
+                # Stops Buddah until newt START instruction
+                # Takes no parameters
+                if instruction == "stop" and status == RUNNING:
                     m.mark_read()
-                    m.reply("Starting")
-                    print("Recieved START instruction from "+m.author.name)
-                    return True
-        except AttributeError:
-            print("No new messages")
-        except StopIteration:
-            print("End of new messages")
-        time.sleep(60)
+                    m.reply("Sucessfully stoped!")
+                    print("Recieved STOP instruction from "+author)
+                    status = STOPPED
+                    idle()
+                    break
+
+                # Pauses Buddah for a defined time (seconds by default)
+                # PAUSE [time] *[s, m, h]
+                elif instruction == "pause" and status == RUNNING:
+                    if len(command)>1:
+                        try:
+                            t=int(command[1])
+                            if len(command)>2:
+                                t*= 1*(command[2]=="s") + 60*(command[2]=="m") + 3600*(command[2]=="h")
+                                if t <= 0:
+                                    messageError(m, "PAUSE", "wrong time formating")
+                            if t<=0:
+                                messageError(m, "PAUSE", "time must be greater than 0")
+
+                            m.reply("Sucessfully paused")
+                            m.mark_read()
+                            print("Recieved PAUSE instruction from "+author)
+                            status = PAUSED
+                            idle(t)
+                        except ValueError:
+                            messageError(m, "PAUSE", "no integer")
+                        
+                    else:
+                       messageError(m, "PAUSE", "command too short") 
+
+                # Asks Buddah what status it is in (RUNNING, STOPPED, or PAUSED)
+                # Takes no parameters
+                elif instruction == "status":
+                    m.reply("Current status: " + status  + (arg>=0 and status == PAUSED)*(" ("+str(arg)+" s remaining)"))
+                    m.mark_read()
+                    print("Sent status information to " + author)
+
+                # Restarts Buddah
+                # Takes no parameters
+                elif instruction == "start":
+                    if status in [STOPPED, PAUSED]:
+                        m.reply("Sucessfully started")
+                        m.mark_read()
+                        print("Recieved START instruction from "+author)
+                        return "RESTART"
+                    else:
+                        messageError(m, "START", "Buddah is already running !")
+                    
+                # Clears command queue
+                # Takes no parameters
+                elif instruction == "clear":
+                    queue = reddit.inbox.unread()
+                    for e in queue:
+                        e.mark_read()
+                    m.reply("Sucessfully cleared queue")
+                    print("Recieved CLEAR instruction from "+author)
+                    break
+
+                     
+    except AttributeError:
+        pass
+    except StopIteration:
+        pass
+
+
+def idle(t=-1):
+    global status
+    while True:
+        restart = checkForCommands(t)
+        if restart == "RESTART":
+            status=RUNNING
+            return True
+        if t==-1:
+            time.sleep(message_refresh)
+        else:
+            if t>=message_refresh:
+                time.sleep(message_refresh)
+                t-=message_refresh
+            else:
+                time.sleep(t)
+                status = RUNNING
+                return True
         
 
 def quickSort(lst):
@@ -135,4 +216,7 @@ def containsFilter(string):
 def postedInFilteredSub(post):
     return post.subreddit.display_name in subs
 
-idle()
+def messageError(m, s, t):  #MST lol
+                        m.reply("Failed to execute "+ s + "instruction: " +t)
+                        m.mark_read()
+                        print("Failed to execute "+ s + " instruction from "+m.author.name+ ": "+t)
